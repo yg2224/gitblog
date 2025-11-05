4d5a9686fb628b10d2fb2f391e5cd842805d773a
geng yue <136247805+yg2224@users.noreply.github.com>
2025-11-05 15:31:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
 MD_HEAD = """## [Gitblog](https://yihong0618.github.io/gitblog/)
-My personal blog([About Me](https://github.com/yihong0618/gitblog/issues/282)) using issues and GitHub Actions (随意转载，无需署名)
+My personal blog([About Me](https://github.com/yg2224/gitblog/issues/282)) using issues and GitHub Actions (随意转载，无需署名)
 [Things I like](https://github.com/yihong0618/gitblog/issues/311)
 ![image](https://github.com/user-attachments/assets/a168bf11-661e-4566-b042-7fc9544de528)
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
